632784e6267b3d938997ca10c43150e5db3a8ad4
orafaelcarvalho <rsouzadecarvalho@gmail.com>
2022-12-13 20:16:05 -0300

    Up

---
+++ UsuariosApi/Controllers/CadastroController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UsuariosApi.Data.Dtos;
+
+namespace UsuariosApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CadastroController : ControllerBase
+    {
+
+        [HttpPost]
+        public IActionResult CadastraUsuario(CreateUsuarioDto createDto)
+        {
+            // TODO chamar o service
+            return Ok();
+        }
+    }
+}
---
+++ UsuariosApi/Models/Usuario.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace UsuariosApi.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+    }
+}
